64caf7200f4e0a5add468e8de495a2fb6601a4a7
Karasukaigan <karasukaigan@gmail.com>
2021-05-19 18:17:25 +0900

    1.1.0

--- getBilibiliUserFans.py
+++ getBilibiliUserFans.py
@@ -12,11 +12,9 @@ import json
 def main():
     uid = 433351  # 某只鹅的UID
     responseData = getUserData(uid)  # 该函数返回一个包含用户信息的字典
-    userData = responseData['data']  # 用户数据，包含信息卡、粉丝量等信息
-    userCard = userData['card']  # 用户信息卡，包含用户ID（昵称）、签名等信息
 
-    print("B站ID：" + userCard['name'])
-    print("粉丝数：" + str(userData['follower']))
+    print("B站ID：" + getUserID(responseData))
+    print("粉丝数：" + str(getFansCount(responseData)))
 
 
 def getUserData(uid):
@@ -25,8 +23,18 @@ def getUserData(uid):
         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36", }
 
     response = requests.get(url, headers=headers)
-
     return json.loads(response.text)
+
+
+def getFansCount(responseData):
+    userData = responseData['data']  # 用户数据，包含信息卡、粉丝量等信息
+    return userData['follower']
+
+
+def getUserID(responseData):
+    userData = responseData['data']
+    userCard = userData['card']  # 用户信息卡，包含用户ID（昵称）、签名等信息
+    return userCard['name']
 
 
 if __name__ == "__main__":
