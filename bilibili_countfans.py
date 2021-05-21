592fe6c5255b11604c97493690f4f9949adbe20a
Karasukaigan <karasukaigan@gmail.com>
2021-05-21 18:42:23 +0900

    1.1.0

--- bilibili_countfans.py
+++ bilibili_countfans.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 # @Time : 2021/5/19 16:03
 # @Author : Karasukaigan
-# @File : getBilibiliUserFans.py
+# @File : bilibili_countfans.py
 # @Software : PyCharm
 
 
@@ -11,13 +11,13 @@ import json
 
 def main():
     uid = 433351  # 某只鹅的UID
-    responseData = getUserData(uid)  # 该函数返回一个包含用户信息的字典
+    responseData = get_user_data(uid)  # 该函数返回一个包含用户信息的字典
 
-    print("B站ID：" + getUserID(responseData))
-    print("粉丝数：" + str(getFansCount(responseData)))
+    print("B站ID：" + get_uid(responseData))
+    print("粉丝数：" + str(get_fans(responseData)))
 
 
-def getUserData(uid):
+def get_user_data(uid):
     url = r"https://api.bilibili.com/x/web-interface/card?jsonp=jsonp&mid=" + str(uid)
     headers = {
         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36", }
@@ -26,12 +26,12 @@ def getUserData(uid):
     return json.loads(response.text)
 
 
-def getFansCount(responseData):
+def get_fans(responseData):
     userData = responseData['data']  # 用户数据，包含信息卡、粉丝量等信息
     return userData['follower']
 
 
-def getUserID(responseData):
+def get_uid(responseData):
     userData = responseData['data']
     userCard = userData['card']  # 用户信息卡，包含用户ID（昵称）、签名等信息
     return userCard['name']
